846f2bb4f4fca41a129020326542686184b2a982
anhduyhtml <daivuongsaohoa3@gmail.com>
2017-03-17 00:38:27 +0700

    Tuan2

---
+++ src/main/java/com/example/HellostringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellostringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellostringbootApplication.class, args);
+	}
+}
